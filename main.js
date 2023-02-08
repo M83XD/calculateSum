d3987d69d9d3163baec3a9ce2330f8e5b891efdc
M83 <75308710+M83XD@users.noreply.github.com>
2023-02-08 23:56:27 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+<script>
+   function calculateSum() {
+      var num1 = parseInt(prompt("1. Numarayı giriniz : "));
+      var num2 = parseInt(prompt("2. Numarayı giriniz : "));
+      var sum = num1 + num2;
+      alert("The sum is: " + sum);
+   }
+</script>
+
+<button onclick="calculateSum()">Tıkla !</button>
